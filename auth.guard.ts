a8829f42e2c42bf5203b7ef516044bbb3c263c33
Sharanya9102003 <sharanyaedulakanti9@gmail.com>
2025-07-30 10:33:59 +0530

    Update auth.guard.ts

--- auth.guard.ts
+++ auth.guard.ts
@@ -1,1 +1,23 @@
-
+import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+ 
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const userName= localStorage.getItem('userName');
+  // console.log(userName);
+  if (userName) {
+ 
+ 
+    // router.navigate(['']);
+    return true;
+  } else {
+    router.navigate(['']);
+    return false;
+ 
+  }
+};
+ 
+ 
+ 
+ 
