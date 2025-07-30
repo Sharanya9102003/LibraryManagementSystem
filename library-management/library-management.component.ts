00b5214891ade2b560b6462cd08cffeb9e48f9f5
Sharanya9102003 <sharanyaedulakanti9@gmail.com>
2025-07-30 10:09:07 +0530

    Add files via upload

---
+++ library-management/library-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-library-management',
+  imports: [],
+  templateUrl: './library-management.component.html',
+  styleUrl: './library-management.component.css'
+})
+export class LibraryManagementComponent {
+
+}
